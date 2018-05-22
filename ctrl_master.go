9ceaa03248a8e6cb1d4b749e48b0eae03d4a3926
Rob <robertely@gmail.com>
2018-05-21 23:47:14 -0600

    do the du

--- ctrl_master.go
+++ ctrl_master.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/kr/pty"
 	"golang.org/x/crypto/ssh/terminal"
@@ -54,50 +54,66 @@ func (cm *ControlMaster) Open() {
 	terminal.MakeRaw(int(cm.ptmx.Fd()))
 }
 
-func (cm *ControlMaster) sendCtrlCmd(ctrlcmd string) bool {
+func (cm *ControlMaster) sendCtrlCmd(ctrlcmd string) string {
 	name := "ssh"
-	args := []string{"-oControlPath=" + cm.socketPath, cm.targetHost, "-p", cm.targetPort, "-O " + ctrlcmd}
+	args := []string{"-oControlPath=" + cm.socketPath, cm.targetHost, "-p", cm.targetPort, "-O", ctrlcmd}
 	cmd := exec.Command(name, args...)
-	out, err := cmd.CombinedOutput()
+	stdout, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err.Error())
+		return ""
 	}
-	fmt.Println(string(out))
-	if strings.HasPrefix(string(out), "Master running") {
-		return true
-	}
-	return false
-
+	return string(stdout)
 }
 
 func (cm *ControlMaster) Close() {
-	cm.sendCtrlCmd("check")
+	cm.ptmx.Close()
 }
 
 func (cm ControlMaster) Send(s string) {
 	cm.ptmx.Write([]byte(s))
 }
 
-// Check - ssh ctl_cmd
-// (check that the master process is running)
-func (cm ControlMaster) Check() bool {
-	return cm.sendCtrlCmd("check")
-}
-
-// Exit - ssh ctl_cmd
-// (request the master to	exit)
-func (cm ControlMaster) Exit() {
-	fmt.Println("Exit")
-}
-
-// Stop - ssh ctl_cmd
-// (request the master to stop accepting further multiplexing requests)
-func (cm ControlMaster) Stop() {
-	fmt.Println("Stop")
-}
-
 // Kill - Signal sigKill to the ControlMaster process
 //   sigKill ssh control master
 func (cm ControlMaster) Kill() {
 	cm.cmd.Process.Signal(os.Kill)
+}
+
+// Check - ssh ctl_cmd
+// (check that the master process is running)
+func (cm ControlMaster) Check() bool {
+	stdout := cm.sendCtrlCmd("check")
+	if strings.HasPrefix(string(stdout), "Master running") {
+		return true
+	}
+	return false
+}
+
+func (cm ControlMaster) ReadyWait() {
+	for !cm.Check() {
+		time.Sleep(50 * time.Millisecond)
+	}
+	return
+}
+
+// Exit - ssh ctl_cmd
+// (request the master to	exit)
+func (cm ControlMaster) Exit() bool {
+	stdout := cm.sendCtrlCmd("exit")
+	if strings.HasPrefix(string(stdout), "Exit request sent.") {
+		return true
+	}
+	return false
+}
+
+// Stop - ssh ctl_cmd
+// (request the master to stop accepting further multiplexing requests)
+func (cm ControlMaster) Stop() bool {
+	stdout := cm.sendCtrlCmd("stop")
+	fmt.Println(stdout)
+	if strings.HasPrefix(stdout, "Stop listening request sent.") {
+		return true
+	}
+	return false
 }
--- main.go
+++ main.go
@@ -1,25 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"time"
+	"os"
 )
 
 func main() {
 	master := NewControlMaster("192.168.1.238", "22", "sshotgun-%h-%p-%C.sock")
 	master.Open()
 	defer master.Close()
-	defer master.Kill()
 
-	time.Sleep(3 * time.Second)
+	master.ReadyWait()
 	if master.Check() {
 		fmt.Println("READY")
 	} else {
 		fmt.Println("NOT READY")
 	}
-
-	// scanner := bufio.NewScanner(master.ptmx)
-	// for scanner.Scan() {
-	// 	fmt.Fprintf(os.Stdout, "\033[0;36mRx\033[0m: %s\n", scanner.Text())
-	// }
+	scanner := bufio.NewScanner(master.ptmx)
+	for scanner.Scan() {
+		fmt.Fprintf(os.Stdout, "\033[0;36mRx\033[0m: %s\n", scanner.Text())
+	}
 }
